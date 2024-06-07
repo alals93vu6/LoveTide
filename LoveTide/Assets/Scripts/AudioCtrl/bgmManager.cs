d166ae2d823222c012d47c39b13e447a2fcb6164
alals93vu6 <a0987915131@gmail.com>
2024-06-08 03:09:25 +0800

    Add BGMmanager

---
+++ LoveTide/Assets/Scripts/AudioCtrl/bgmManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgmManager : MonoBehaviour
+{
+    #region Instance
+    static public bgmManager instance;
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+    #endregion
+
+    [SerializeField] public AudioSource[] backgroundMusic;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayAudio(int audioNumber)
+    {
+        Debug.Log(audioNumber);
+    }
+}
