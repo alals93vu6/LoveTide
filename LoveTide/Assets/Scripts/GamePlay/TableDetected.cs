909395e9d72f5ca4998ea5c9260eb5de9c98d3ea
alals93vu6 <a0987915131@gmail.com>
2024-07-01 03:05:51 +0800

    Table Detected

---
+++ LoveTide/Assets/Scripts/GamePlay/TableDetected.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableDetected : MonoBehaviour
+{
+    [SerializeField] private GameObject yukaObject;
+    [SerializeField] private bool detected;
+    // Update is called once per frame
+    void Update()
+    {
+        YukaDetected();
+    }
+
+    private void YukaDetected()
+    {
+        if (yukaObject.gameObject.transform.position.y >= 210)
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 6;
+            detected = true;
+        }
+        else
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 4;
+            detected = false;
+        }
+    }
+}
