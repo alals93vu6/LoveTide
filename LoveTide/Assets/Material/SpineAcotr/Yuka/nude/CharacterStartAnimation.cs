309cbda16a263220d237f6b0be8ea13ba201609a
alals93vu6 <a0987915131@gmail.com>
2025-04-29 02:59:19 +0800

    Update Yuka Spine Object 3.0

---
+++ LoveTide/Assets/Material/SpineAcotr/Yuka/nude/CharacterStartAnimation.cs
@@ -0,0 +1,32 @@
+﻿using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStartAnimation : MonoBehaviour
+{
+    [SpineAnimation] public string bodyIdleAnimation = "body_idle";
+    [SpineAnimation] public string eyesIdleAnimation = "Eyes_Nomal";
+    [SpineAnimation] public string mouthIdleAnimation = "Mouth_Happy";
+    [SpineAnimation] public string facIdleAnimation = "FaceBottoms_Blush";
+
+    private SkeletonAnimation skeletonAnimation;
+
+    private void Start()
+    {
+        skeletonAnimation = GetComponent<SkeletonAnimation>();
+        if (skeletonAnimation == null)
+        {
+            Debug.LogError("找不到 SkeletonAnimation 元件！");
+            return;
+        }
+
+        var state = skeletonAnimation.AnimationState;
+
+        // 在不同的 Track 撥放不同部位動畫
+        state.SetAnimation(0, bodyIdleAnimation, true);   // 身體播放在 Track 0
+        state.SetAnimation(1, eyesIdleAnimation, true);   // 眼睛播放在 Track 1
+        state.SetAnimation(2, mouthIdleAnimation, true);  // 嘴巴播放在 Track 2
+        state.SetAnimation(3, facIdleAnimation, true);
+    }
+}
